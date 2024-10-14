6cb7e7e611e6616184a77797f79c09415638fc2c
Mirza Husic <72878893+husicmirza@users.noreply.github.com>
2024-10-15 00:55:02 +0300

    Feat: Add new expandable sidebar that expands on hover (#1)

---
+++ components/DashboardSidebar.tsx
@@ -0,0 +1,86 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import { Sidebar, SidebarBody, SidebarLink } from "./ui/sidebar";
+
+import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import DashboardFooter from "./DashboardFooter";
+
+const DashboardSidebar = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div
+      className={cn(
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  border border-neutral-200 dark:border-neutral-700 overflow-hidden h-screen"
+      )}
+    >
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            {open ? <Logo /> : <LogoIcon />}
+            <div className="mt-8 flex flex-col gap-2">
+              {sidebarLinks.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
+            </div>
+          </div>
+          <DashboardFooter />
+        </SidebarBody>
+      </Sidebar>
+      <div className="flex flex-1">
+        <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+const Logo = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <Image
+        src="/assets/logo.png"
+        width={1000}
+        height={1000}
+        alt="Gcc logo"
+        className="h-8 w-8"
+      />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-neutral-700 dark:text-white whitespace-pre"
+      >
+        GCC Netwokring
+      </motion.span>
+    </Link>
+  );
+};
+const LogoIcon = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <Image
+        src="/assets/logo.png"
+        width={1000}
+        height={1000}
+        alt="Gcc logo"
+        className="h-8 w-8"
+      />
+    </Link>
+  );
+};
+
+export default DashboardSidebar;
