cecd3e00cf24985de9ab70d5a2a95510b1696cbf
husicmirza <mirza.huc@gmail.com>
2024-10-16 14:33:28 +0300

    Add floating navigation

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,18 +1,24 @@
 import type { Metadata } from "next";
 import Footer from "@/components/Footer";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { navItems } from "@/constants";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
 export const metadata: Metadata = {
   title: "Home",
   description: "Gcc Network",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getLoggedInUser();
+
   return (
     <main className="flex flex-col min-h-screen">
+      <FloatingNav navItems={navItems} user={user} />
       <div className="flex-grow">{children}</div>
       <Footer />
     </main>
--- constants/index.tsx
+++ constants/index.tsx
@@ -3,35 +3,48 @@ import {
   IconDashboard,
   IconUsers,
   IconUserBolt,
+  IconHome,
+  IconBlockquote,
+  IconMessage,
 } from "@tabler/icons-react";
 
 export const sidebarLinks = [
   {
     label: "Dashboard",
     href: "/dashboard",
-    icon: (
-      <IconDashboard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
+    icon: <IconDashboard className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
   },
   {
     label: "Profile",
     href: "/dashboard/profile",
-    icon: (
-      <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
+    icon: <IconUserBolt className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
   },
   {
     label: "People",
     href: "/dashboard/people",
-    icon: (
-      <IconUsers className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
+    icon: <IconUsers className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
   },
   {
     label: "Companies",
     href: "/dashboard/companies",
-    icon: (
-      <IconBuildings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
+    icon: <IconBuildings className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
+  },
+];
+
+export const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <IconHome className="h-5 w-5 text-neutral-700" />,
+  },
+  {
+    name: "Blog",
+    link: "/posts",
+    icon: <IconBlockquote className="h-5 w-5 text-neutral-700" />,
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+    icon: <IconMessage className="h-5 w-5 text-neutral-700" />,
   },
 ];
