a75088d9f78e9093653946b0696bb385c5e5ac7f
Mirza Husic <72878893+husicmirza@users.noreply.github.com>
2024-10-16 15:51:25 +0300

    Add footer and navigation components with logout functionality (#2)

---
+++ components/ui/floating-navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import AuthButton from "../AuthButton";
+
+export const FloatingNav = ({
+  navItems,
+  className,
+  user,
+}: {
+  navItems: {
+    label: string;
+    href: string;
+    icon?: JSX.Element;
+  }[];
+  className?: string;
+  user: LoggedInUser | null;
+}) => {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        className={cn(
+          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center sm:space-x-8 space-x-4",
+          className
+        )}
+      >
+        {navItems.map((navItem: any, idx: number) => (
+          <Link
+            key={`link=${idx}`}
+            href={navItem.href}
+            className={cn(
+              "relative  items-center flex text-neutral-700 hover:text-neutral-500"
+            )}
+          >
+            <div className="flex items-center space-x-1">
+              <span className="block ">{navItem.icon}</span>
+              <span className="hidden sm:block text-sm">{navItem.label}</span>
+            </div>
+          </Link>
+        ))}
+        <AuthButton user={user} />
+      </motion.div>
+    </AnimatePresence>
+  );
+};
--- constants/index.tsx
+++ constants/index.tsx
@@ -3,35 +3,67 @@ import {
   IconDashboard,
   IconUsers,
   IconUserBolt,
+  IconHome,
+  IconBlockquote,
+  IconMessage,
 } from "@tabler/icons-react";
 
 export const sidebarLinks = [
   {
     label: "Dashboard",
     href: "/dashboard",
-    icon: (
-      <IconDashboard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
+    icon: <IconDashboard className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
   },
   {
     label: "Profile",
     href: "/dashboard/profile",
-    icon: (
-      <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
+    icon: <IconUserBolt className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
   },
   {
     label: "People",
     href: "/dashboard/people",
-    icon: (
-      <IconUsers className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
+    icon: <IconUsers className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
   },
   {
     label: "Companies",
     href: "/dashboard/companies",
+    icon: <IconBuildings className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
+  },
+];
+
+export const navItems = [
+  {
+    label: "Home",
+    href: "/",
     icon: (
-      <IconBuildings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      <IconHome className="h-5 w-5 text-neutral-700 hover:text-neutral-500" />
     ),
+  },
+  {
+    label: "Blog",
+    href: "/posts",
+    icon: (
+      <IconBlockquote className="h-5 w-5 text-neutral-700 hover:text-neutral-500" />
+    ),
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+    icon: (
+      <IconMessage className="h-5 w-5 text-neutral-700 hover:text-neutral-500" />
+    ),
+  },
+];
+
+export const menuItems = [
+  {
+    label: "Profile",
+    href: "/dashboard/profile",
+    icon: <IconUserBolt className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
+  },
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+    icon: <IconDashboard className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
   },
 ];
