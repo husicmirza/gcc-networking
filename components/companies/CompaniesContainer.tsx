7bf426fdb2d730a5241126e17d4cbb9092ab2d3c
husicmirza <mirza.huc@gmail.com>
2024-11-02 22:00:38 +0300

    Add filter for comanies

--- components/companies/CompaniesContainer.tsx
+++ components/companies/CompaniesContainer.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
 import { companyItems } from "@/constants";
+import CompaniesFilters from "./CompaniesFilters";
+import { useSearchParams } from "next/navigation";
 
 const CompaniesContainer = () => {
+  const searchParams = useSearchParams();
+  const [searchTerm, setSearchTerm] = useState(searchParams.get("query") || "");
+
   return (
-    <div className="flex h-full space-y-8 flex-col">
+    <div className="flex h-full space-y-8 flex-col max-w-4xl mx-auto">
       <div className="border-b pb-2">
         <h2 className="text-2xl font-bold tracking-tight">Hi there! 👋</h2>
         <p className="text-muted-foreground">
           Here&apos;s a list of Balkan Companies in the GCC!
         </p>
       </div>
+      <CompaniesFilters searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <BentoGrid className="max-w-4xl mx-auto">
         {companyItems.map((item, i) => (
           <BentoGridItem
---
+++ components/companies/CompaniesFilters.tsx
@@ -0,0 +1,170 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+import {
+  Select,
+  SelectValue,
+  SelectTrigger,
+  SelectItem,
+  SelectContent,
+} from "../ui/select";
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+const industryOptions = [
+  {
+    value: "all",
+    label: "All",
+  },
+  {
+    value: "it",
+    label: "IT",
+  },
+  {
+    value: "medicine",
+    label: "Medicine",
+  },
+];
+const countryOptions = [
+  {
+    value: "all",
+    label: "All",
+  },
+  {
+    value: "qatar",
+    label: "Qatar",
+  },
+  {
+    value: "uae",
+    label: "UAE",
+  },
+];
+
+interface CompaniesFiltersProps {
+  searchTerm: string;
+  setSearchTerm: (searchTerm: string) => void;
+}
+const CompaniesFilters = ({
+  searchTerm,
+  setSearchTerm,
+}: CompaniesFiltersProps) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+
+  const [industry, setIndustry] = useState(
+    searchParams.get("industry") || "all"
+  );
+  const [country, setCountry] = useState(searchParams.get("country") || "all");
+  console.log(industry);
+  const updateFilterParams = (): void => {
+    const params = new URLSearchParams(searchParams);
+    console.log(params);
+    const updateParam = (paramName: string, stateValue: string | boolean) => {
+      params.set(paramName, stateValue.toString());
+    };
+
+    updateParam("industry", industry);
+    updateParam("country", country);
+
+    if (searchTerm) {
+      params.set("query", searchTerm);
+    } else {
+      params.delete("query");
+    }
+
+    replace(`?${params.toString()}`);
+  };
+  const resetFilters = (): void => {
+    const isAlreadyDefault =
+      searchParams.get("query") === "" &&
+      industry === "all" &&
+      country === "all" &&
+      !searchParams.has("industry") &&
+      !searchParams.has("country");
+
+    if (isAlreadyDefault) {
+      return;
+    }
+
+    setIndustry("all");
+    setCountry("all");
+    setSearchTerm("");
+
+    const params = new URLSearchParams(searchParams);
+    params.delete("industry");
+    params.delete("country");
+    params.delete("query");
+    replace(`?${params.toString()}`);
+  };
+
+  return (
+    <div className="flex w-full justify-between">
+      <div className="flex gap-4">
+        <div className="flex flex-col gap-2">
+          <Label className="text-muted-foreground">Search</Label>
+          <Input
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label className="text-muted-foreground">Industry</Label>
+          <Select
+            onValueChange={(e: any) => setIndustry(e)}
+            value={industry}
+            defaultValue={industry}
+          >
+            <SelectTrigger className="w-48">
+              <SelectValue placeholder="Select an industry" />
+            </SelectTrigger>
+            <SelectContent>
+              {industryOptions.map((option) => (
+                <SelectItem value={option.value} key={option.value}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label className="text-muted-foreground">Country</Label>
+          <Select
+            onValueChange={(e: any) => setCountry(e)}
+            value={country}
+            defaultValue={country}
+          >
+            <SelectTrigger className="w-48">
+              <SelectValue placeholder="Select a country" />
+            </SelectTrigger>
+            <SelectContent>
+              {countryOptions.map((option) => (
+                <SelectItem value={option.value} key={option.value}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <div className="flex justify-end items-end gap-2 pt-6 lg:ml-2 lg:pt-0">
+        <Button
+          variant="default"
+          size="sm"
+          onClick={() => {
+            updateFilterParams();
+          }}
+        >
+          Apply
+        </Button>
+        <div className="ml-2 hidden lg:flex">
+          <Button variant="secondary" size="sm" onClick={resetFilters}>
+            Reset
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompaniesFilters;
