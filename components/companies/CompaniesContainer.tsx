14335c6d02580d6d34e7b08740eea4e549d9768c
husicmirza <mirza.huc@gmail.com>
2024-11-02 15:35:41 +0300

    Add comapnies items

--- components/companies/CompaniesContainer.tsx
+++ components/companies/CompaniesContainer.tsx
@@ -1,76 +1,28 @@
 import React from "react";
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
-import {
-  IconArrowWaveRightUp,
-  IconBoxAlignRightFilled,
-  IconBoxAlignTopLeft,
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
-} from "@tabler/icons-react";
+import { companyItems } from "@/constants";
 
 const CompaniesContainer = () => {
   return (
-    <BentoGrid className="max-w-4xl mx-auto">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          icon={item.icon}
-        />
-      ))}
-    </BentoGrid>
+    <div className="flex h-full space-y-8 flex-col">
+      <div className="border-b pb-2">
+        <h2 className="text-2xl font-bold tracking-tight">Hi there! 👋</h2>
+        <p className="text-muted-foreground">
+          Here&apos;s a list of Balkan Companies in the GCC!
+        </p>
+      </div>
+      <BentoGrid className="max-w-4xl mx-auto">
+        {companyItems.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            badge={item.badge}
+          />
+        ))}
+      </BentoGrid>
+    </div>
   );
 };
 export default CompaniesContainer;
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-);
-const items = [
-  {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Pursuit of Knowledge",
-    description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton />,
-    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Joy of Creation",
-    description: "Experience the thrill of bringing ideas to life.",
-    header: <Skeleton />,
-    icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Spirit of Adventure",
-    description: "Embark on exciting journeys and thrilling discoveries.",
-    header: <Skeleton />,
-    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-  },
-];
--- constants/index.tsx
+++ constants/index.tsx
@@ -7,6 +7,7 @@ import {
   IconBlockquote,
   IconMessage,
 } from "@tabler/icons-react";
+import Image from "next/image";
 
 export const sidebarLinks = [
   {
@@ -65,5 +66,63 @@ export const menuItems = [
     label: "Dashboard",
     href: "/dashboard",
     icon: <IconDashboard className="text-neutral-700 h-5 w-5 flex-shrink-0" />,
+  },
+];
+
+export const companyItems = [
+  {
+    title: "Techman Solutions",
+    description:
+      "We specialize in designing, implementing, and maintaining cutting-edge ICT and ELV solutions, including IP-based systems and network infrastructure.",
+    header: (
+      <Image
+        src="/assets/full-logo.png"
+        width={1000}
+        height={1000}
+        alt="Gcc logo"
+      />
+    ),
+    badge: "IT",
+  },
+  {
+    title: "Techman Solutions",
+    description:
+      "We specialize in designing, implementing, and maintaining cutting-edge ICT and ELV solutions, including IP-based systems and network infrastructure.",
+    header: (
+      <Image
+        src="/assets/medax-logo.png"
+        width={1000}
+        height={1000}
+        alt="Gcc logo"
+      />
+    ),
+    badge: "Farmacy",
+  },
+  {
+    title: "Techman Solutions",
+    description: "We specialize in designing, implementing, and maintaining.",
+    header: (
+      <Image
+        src="/assets/odoo-logo.png"
+        width={1000}
+        height={1000}
+        alt="Gcc logo"
+      />
+    ),
+    badge: "Software and Hardware",
+  },
+  {
+    title: "Techman Solutions",
+    description:
+      "maintaining cutting-edge ICT and ELV solutions, including IP-based systems and network infrastructure.",
+    header: (
+      <Image
+        src="/assets/free-logo.svg"
+        width={1000}
+        height={1000}
+        alt="Gcc logo"
+      />
+    ),
+    badge: "Hospitality",
   },
 ];
