c8f9351e8f88542be7124f89b81bf57811349f5f
husicmirza <mirza.huc@gmail.com>
2024-11-04 13:59:51 +0300

    Fix build

--- app/(secure)/dashboard/people/page.tsx
+++ app/(secure)/dashboard/people/page.tsx
@@ -1,6 +1,7 @@
-
 import PeopleContainer from "@/components/people/PeopleContainer";
 
-export default function PeoplePage({ searchParams }: SearchParamProps) {
-  return <PeopleContainer searchParams={searchParams} />;
-}
+const PeoplePage = () => {
+  return <PeopleContainer />;
+};
+
+export default PeoplePage;
