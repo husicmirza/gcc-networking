ad65881a7756e4e834da9c44c1da10428da38eb6
husicmirza <mirza.huc@gmail.com>
2024-11-04 13:55:27 +0300

    fix layout

--- components/people/PeopleContainer.tsx
+++ components/people/PeopleContainer.tsx
@@ -5,7 +5,7 @@ import { peopleItems } from "@/constants";
 
 const PeopleContainer = () => {
   return (
-    <div className="flex space-y-8 flex-col max-w-4xl mx-auto w-full">
+    <div className="flex space-y-8 flex-col max-w-5xl mx-auto w-full">
       <div className="border-b pb-2">
         <h2 className="text-2xl font-bold tracking-tight">Hi there! 👋</h2>
         <p className="text-muted-foreground">
