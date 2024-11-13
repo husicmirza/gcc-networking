d6273fc987e94428598a6d134a7eb149ea04bf7c
husicmirza <mirza.huc@gmail.com>
2024-11-13 12:40:34 +0300

    refactoring public user action

--- lib/actions/publicUsers.actions.ts
+++ lib/actions/publicUsers.actions.ts
@@ -1,42 +1,18 @@
 "use server";
 import { ID } from "node-appwrite";
 import { createAdminClient } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { formatUserData, parseStringify } from "../utils";
 import { User } from "@/types/appwrite.types";
 
 const { DATABASE_ID, PUBLIC_USERS_COLLECTION_ID } = process.env;
 export const createPublicUser = async ({ userData }: { userData: User }) => {
-  const newUserData = {
-    userId: userData.userId,
-    email: userData.email,
-    firstName: userData.firstName,
-    lastName: userData.lastName,
-    dateOfBirth: userData.dateOfBirth,
-    address1: userData.address1,
-    city: userData.city,
-    country: userData.country,
-    zipCode: userData.zipCode ?? "",
-    phone: userData.phone,
-    status: userData.status,
-    cancellationReason: userData.cancellationReason ?? "",
-    company: userData.company ?? "",
-    industry: userData.industry ?? "",
-    linkedin: userData.linkedin ?? "",
-    instagram: userData.instagram ?? "",
-    facebook: userData.facebook ?? "",
-    cityOfOrigin: userData.cityOfOrigin ?? "",
-    countryOfOrigin: userData.countryOfOrigin ?? "",
-    image: userData.image ?? "",
-    user: userData.$id ?? "",
-  };
-
   try {
     const { database } = await createAdminClient();
     const newUser = await database.createDocument(
       DATABASE_ID!,
       PUBLIC_USERS_COLLECTION_ID!,
       ID.unique(),
-      newUserData
+      formatUserData(userData)
     );
 
     return parseStringify(newUser);
@@ -51,34 +27,12 @@ export const updatePublicUserInfo = async ({
   userData,
 }: UpdateUserInfoParams) => {
   try {
-    const updateUserData = {
-      userId: userData.userId,
-      email: userData.email,
-      firstName: userData.firstName,
-      lastName: userData.lastName,
-      dateOfBirth: userData.dateOfBirth,
-      address1: userData.address1,
-      city: userData.city,
-      country: userData.country,
-      zipCode: userData.zipCode ?? "",
-      phone: userData.phone,
-      status: userData.status,
-      cancellationReason: userData.cancellationReason ?? "",
-      company: userData.company ?? "",
-      industry: userData.industry ?? "",
-      linkedin: userData.linkedin ?? "",
-      instagram: userData.instagram ?? "",
-      facebook: userData.facebook ?? "",
-      cityOfOrigin: userData.cityOfOrigin ?? "",
-      countryOfOrigin: userData.countryOfOrigin ?? "",
-      image: userData.image ?? "",
-    };
     const { database } = await createAdminClient();
     const user = await database.updateDocument(
       DATABASE_ID!,
       PUBLIC_USERS_COLLECTION_ID!,
       userId,
-      updateUserData
+      formatUserData(userData)
     );
     return parseStringify(user);
   } catch (error) {
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { remark } from "remark";
 import html from "remark-html";
+import { User } from "@/types/appwrite.types";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -10,3 +11,27 @@ export default async function markdownToHtml(markdown: string) {
   const result = await remark().use(html).process(markdown);
   return result.toString();
 }
+
+export const formatUserData = (userData: User) => ({
+  userId: userData.userId,
+  email: userData.email,
+  firstName: userData.firstName,
+  lastName: userData.lastName,
+  dateOfBirth: userData.dateOfBirth,
+  address1: userData.address1,
+  city: userData.city,
+  country: userData.country,
+  zipCode: userData.zipCode ?? "",
+  phone: userData.phone,
+  status: userData.status,
+  cancellationReason: userData.cancellationReason ?? "",
+  company: userData.company ?? "",
+  industry: userData.industry ?? "",
+  linkedin: userData.linkedin ?? "",
+  instagram: userData.instagram ?? "",
+  facebook: userData.facebook ?? "",
+  cityOfOrigin: userData.cityOfOrigin ?? "",
+  countryOfOrigin: userData.countryOfOrigin ?? "",
+  image: userData.image ?? "",
+  user: userData.$id ?? "",
+});
