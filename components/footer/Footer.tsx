7e1aaa1ce492407e217a4bbfee272256c55a82ba
Mirza Husic <72878893+husicmirza@users.noreply.github.com>
2024-11-26 21:10:52 +0300

    Feat: add footer section with links, social media, and logo (#13)

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,18 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+} from "@tabler/icons-react";
+
 const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 ">
-      <div className="container mx-auto px-5">
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Lorem ipsum dolor sit amet
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="/contact"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+      <div className="container mx-auto px-5 py-8">
+        <div className="flex flex-col items-center text-center">
+          <Image
+            className="w-auto h-14 "
+            src="/assets/full-logo.png"
+            alt=""
+            width={500}
+            height={500}
+          />
+
+          <p className="max-w-md mx-auto mt-4 text-neutral-700 text-center">
+            Balkan Connect is a platform for Balkan expats in the Middle East to
+            connect and grow together.
+          </p>
+
+          <div className="flex gap-x-4 mt-4">
+            <Link href="#">
+              <IconBrandInstagram className="text-neutral-700 h-7 w-7 flex-shrink-0" />
+            </Link>
+            <Link href="#">
+              <IconBrandFacebook className="text-neutral-700 h-7 w-7 flex-shrink-0" />
+            </Link>
+            <Link href="#">
+              <IconBrandLinkedin className="text-neutral-700 h-7 w-7 flex-shrink-0" />
+            </Link>
+          </div>
+        </div>
+        <hr className="my-10 border-neutral-200" />
+        <div className="flex flex-col items-center sm:flex-row sm:justify-between">
+          <p className="text-sm text-neutral-500">
+            © 2024 Balkan Connect. All Rights Reserved.
+          </p>
+          <div className="flex mt-3 sm:mt-0">
+            <Link
+              href="#"
+              className="mx-2 text-sm text-neutral-500 transition-colors duration-300 hover:text-neutral-600"
             >
-              Contact Us
-            </a>
+              Teams
+            </Link>
+
+            <Link
+              href="#"
+              className="mx-2 text-sm text-neutral-500 transition-colors duration-300 hover:text-neutral-600"
+            >
+              Privacy
+            </Link>
+
+            <Link
+              href="#"
+              className="mx-2 text-sm text-neutral-500 transition-colors duration-300 hover:text-neutral-600"
+            >
+              Cookies
+            </Link>
           </div>
         </div>
       </div>
